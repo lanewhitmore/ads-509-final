ae40ef5131ed2917a75fd46458e8f628231f25fa
Ivan <ivanchavez@sandiego.edu>
2023-06-11 14:22:10 -0700

    Adding SVC sentiment model for games dataset

--- functions/token_functions.py
+++ functions/token_functions.py
@@ -33,3 +33,21 @@ def clean_tokenize(text, punct_set = punctuation, string_bool = False):
         return tokens_string
 
     return tokens_cleaned
+
+def detect_language(text):
+    if text.strip() == '':
+        return 'unknown'
+    try:
+        return detect(text)
+    except:
+        return 'unknown'
+
+def sentiment_stats(x_train,x_test, y_train, y_test):
+    print ('Size of Training Data ', x_train.shape[0])
+    print ('Size of Test Data ', x_test.shape[0])
+    print ('Distribution of classes in Training Data :')
+    print ('Positive Sentiment ', str(sum(y_train == 1)/ len(y_train) * 100.0))
+    print ('Negative Sentiment ', str(sum(y_train == 0)/ len(y_train) * 100.0))
+    print ('Distribution of classes in Testing Data :')
+    print ('Positive Sentiment ', str(sum(y_test == 1)/ len(y_test) * 100.0))
+    print ('Negative Sentiment ', str(sum(y_test == 0)/ len(y_test) * 100.0))
